672bbb3270f5fe9fc64a7b56169ca834f6564bbc
Durrani27229 <u87262@gmail.com>
2025-02-16 01:20:18 +0500

    responsive

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const url = `http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const getWeather = async () => {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city.value}&aqi=no`)
+        try{ const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city.value}&aqi=no`)
         const data = await response.json();
         
         main.innerHTML += `<div class="cart">
@@ -49,8 +49,11 @@ form.addEventListener("submit", (event) => {
                 <img src="${iconUrl}${data.current.condition.icon}" class="img-fluid" alt="">
             </div>
             <p>${data.current.condition.text}</p>
-        </div>`
-    ;
+        </div>`}
+        catch(err){
+            console.log(err,"There is some error");
+        }
+       
         
     }
     getWeather()
